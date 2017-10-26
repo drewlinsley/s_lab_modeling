Fixed scaling for dropout at test time.

fit_model.py
@@ -34,6 +34,8 @@ def extract_vgg_features(
             project_path,
             '%s.npz' % project_name))
     neural_data = data['data_matrix']
+    if config.round_neural_data:
+        neural_data = np.round(neural_data)
     # TODO: across_session_data_matrix is subtracted version
     images = data['all_images'].astype(np.float32)
 
@@ -42,7 +44,7 @@ def extract_vgg_features(
     neural_data = neural_data[:, channel_check]
 
     # TODO: create AUX dict with each channel's X/Y
-    output_aux = None
+    output_aux = {'loss': config.loss_type}  # None
     rfs = rf_sizes.get_eRFs(model_type)[layer_name]
 
     # 3. Create a output directory if necessary and save a timestamped numpy.
@@ -135,7 +137,8 @@ def extract_vgg_features(
                     r_in=rfs['r_in'],
                     j_in=rfs['j_in'],
                     timesteps=timesteps,
-                    lesions=config.lesions)
+                    lesions=config.lesions,
+                    train=True)
             else:
                 cm_weights = None
 
@@ -224,7 +227,8 @@ def extract_vgg_features(
                     r_in=rfs['r_in'],
                     j_in=rfs['j_in'],
                     timesteps=timesteps,
-                    lesions=config.lesions)
+                    lesions=config.lesions,
+                    train=False)
 
             # Create output layer for N-recording channels
             val_vgg, val_output_activities, _ = ff.pool_ff_interpreter(
@@ -239,7 +243,8 @@ def extract_vgg_features(
             val_loss, _ = loss_utils.loss_interpreter(
                 logits=val_output_activities,
                 labels=val_neural,
-                loss_type=config.loss_type)
+                loss_type=config.loss_type,
+                max_spikes=config.max_spikes)
 
             # Calculate metrics
             val_accuracy = eval_metrics.metric_interpreter(

layers/normalization_functions/contextual_vector_vd.py
@@ -17,15 +17,19 @@ class ContextualCircuit(object):
             model_version='full',
             timesteps=1,
             lesions=None,
+            train=True,
             SRF=1,
             SSN=9,
             SSF=29,
             strides=[1, 1, 1, 1],
             padding='SAME',
             dtype=tf.float32,
+            dropout=0.5,
             return_weights=True):
 
         self.X = X
+        self.dropout = dropout
+        self.train = train
         self.n, self.h, self.w, self.k = [int(x) for x in X.get_shape()]
         self.model_version = model_version
         self.timesteps = timesteps
@@ -380,7 +384,6 @@ class ContextualCircuit(object):
             data=I,
             weight_key=self.weight_dict['I']['r']['weight']
         )
-        I_update = m * self.gate_nl(I_update_input + I_update_recurrent)
         O_update_input = self.conv_2d_op(
             data=self.X,
             weight_key=self.weight_dict['O']['f']['weight']
@@ -389,7 +392,14 @@ class ContextualCircuit(object):
             data=O,
             weight_key=self.weight_dict['O']['r']['weight']
         )
-        O_update = m * self.gate_nl(O_update_input + O_update_recurrent)
+
+        # Calculate gates and apply dropout
+        if self.train:
+            I_update = m * self.gate_nl(I_update_input + I_update_recurrent)
+            O_update = m * self.gate_nl(O_update_input + O_update_recurrent)
+        else:
+            I_update = (1 / self.dropout) * self.gate_nl(I_update_input + I_update_recurrent)
+            O_update = (1 / self.dropout) * self.gate_nl(O_update_input + O_update_recurrent)
 
         # Circuit
         I_summand = self.recurrent_nl(
@@ -429,7 +439,7 @@ class ContextualCircuit(object):
                 [1, 1, 1, self.k],
                 minval=0,
                 maxval=1.),
-                .5),  # variational dropout mask
+                self.dropout),  # variational dropout mask
             tf.float32)
         if reduce_memory:
             print 'Warning: Using FF version of the model.'

layers/normalization_functions/contextual_vector_zoneout.py
@@ -22,10 +22,14 @@ class ContextualCircuit(object):
             SSF=29,
             strides=[1, 1, 1, 1],
             padding='SAME',
+            dropout=0.5,
             dtype=tf.float32,
+            train=True,
             return_weights=True):
 
         self.X = X
+        self.train = train
+        self.dropout = dropout
         self.n, self.h, self.w, self.k = [int(x) for x in X.get_shape()]
         self.model_version = model_version
         self.timesteps = timesteps
@@ -406,7 +410,6 @@ class ContextualCircuit(object):
             data=I,
             weight_key=self.weight_dict['I']['r']['weight']
         )
-        I_update = m * self.gate_nl(I_update_input + I_update_recurrent)
         O_update_input = self.conv_2d_op(
             data=self.X,
             weight_key=self.weight_dict['O']['f']['weight']
@@ -415,7 +418,14 @@ class ContextualCircuit(object):
             data=O,
             weight_key=self.weight_dict['O']['r']['weight']
         )
-        O_update = m * self.gate_nl(O_update_input + O_update_recurrent)
+
+        # Calculate gates and apply dropout
+        if self.train:
+            I_update = m * self.gate_nl(I_update_input + I_update_recurrent)
+            O_update = m * self.gate_nl(O_update_input + O_update_recurrent)
+        else:
+            I_update = (1 / self.dropout) * self.gate_nl(I_update_input + I_update_recurrent)
+            O_update = (1 / self.dropout) * self.gate_nl(O_update_input + O_update_recurrent)
 
         # Circuit
         I_summand = self.recurrent_nl(